b880422e1dcbde4ee0c5f9e355aa19ac82d90903
arnavinarnaware <150098914+arnavinarnaware@users.noreply.github.com>
2023-11-21 23:36:31 -0500

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,13 +1,12 @@
-# visualize.py
 import pandas as pd
 import matplotlib.pyplot as plt
 import mysql.connector
 
-# Replace 'YOUR_API_KEY' with the actual API key
+#Replace 'YOUR_API_KEY' with personal API key
 api_key = 'YOUR_API_KEY'
-symbols = ['AAPL', 'MSFT', 'GOOGL']  # Replace with the symbols from user input
+symbols = ['AAPL', 'MSFT', 'GOOGL']  # Replace with stock symbols
 
-# Connect to the MySQL database
+#Connect to MySQL database
 connection = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -18,10 +17,10 @@ connection = mysql.connector.connect(
 if connection.is_connected():
     cursor = connection.cursor()
 
-    # Fetch data from the database
+    #Get data from database
     df = pd.read_sql("SELECT * FROM stock_prices WHERE symbol IN (%s, %s, %s)", params=tuple(symbols), con=connection, parse_dates=['date'])
 
-    # Visualization
+    #visualization of prices
     for symbol in symbols:
         df_symbol = df[df['symbol'] == symbol]
         plt.plot(df_symbol['date'], df_symbol['closing_price'], label=symbol)
