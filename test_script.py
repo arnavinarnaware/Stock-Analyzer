ab9a5c4d7fbaa5943c165d1e6fa3ae686ed6dd27
arnavinarnaware <150098914+arnavinarnaware@users.noreply.github.com>
2023-11-21 23:37:48 -0500

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,13 +1,11 @@
 from stock_analysis import get_stock_prices, insert_data_into_mysql
 
-# Get user input for the Alpha Vantage API key
 api_key = input("Enter your Alpha Vantage API key: ")
 
-# Get user input for the stock symbols (comma-separated)
+#Separate stock symbols with commas
 symbols_input = input("Enter the stock symbols (comma-separated): ")
 symbols = [symbol.strip().upper() for symbol in symbols_input.split(',')]
 
-# Call the function to fetch and insert stock prices for each symbol
 for symbol in symbols:
     stock_prices = get_stock_prices(symbol, api_key)
     if stock_prices is not None:
